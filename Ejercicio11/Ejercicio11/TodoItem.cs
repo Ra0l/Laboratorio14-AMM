fec4608341a0befa1f66d3abb5523943738059bd
raul.aliaga.s@tecsup.edu.pe <raul.aliaga.s@tecsup.edu.pe>
2019-11-18 15:34:55 -0500

    primer commit

---
+++ Ejercicio11/Ejercicio11/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio11
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public int Dni { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Fechanacimiento { get; set; }
+        public bool Done { get; set; }
+    }
+}
